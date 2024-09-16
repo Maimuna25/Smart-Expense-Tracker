edb9c46d80b02afe87d9e73376824f5fef33c753
Maimuna25 <144927668+Maimuna25@users.noreply.github.com>
2024-09-16 15:31:02 +0100

    Main menu which gives users the option to choose to add or view their expenses

---
+++ main.py
@@ -0,0 +1,28 @@
+from expense_tracker import add_expense, view_expenses
+from insights import expense_insights
+
+def menu():
+    while True:
+        print("\nExpense Tracker Menu")
+        print("1. Add Expense")
+        print("2. View Expenses")
+        print("3. Expense Insights")
+        print("4. Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            add_expense()
+        elif choice == '2':
+            view_expenses()
+        elif choice == '3':
+            expense_insights()
+        elif choice == '4':
+            print("Exiting the program.")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+# Run the menu
+if __name__ == "__main__":
+    menu()
